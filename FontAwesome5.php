9b7b7eb2427d86c11d3aba4f7963f1bebf026b4d
Balancer <balancer@balancer.ru>
2018-04-02 09:30:12 +0300

    First worked.

--- FontAwesome5.php
+++ FontAwesome5.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace B2\\Chars;
+namespace B2\Chars;
 
-class FontAwesome5
+class FontAwesome5 extends \B2\Chars
 {
 	var $edit_char = '<i class="far fa-edit"></i>';
 	function _edit_str_def() { return '<i class="far fa-edit"></i> '._('Edit'); }
