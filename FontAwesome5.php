0854620ba4231641ccb7564e2284d288092c7150
Balancer <balancer@balancer.ru>
2018-04-02 09:14:02 +0300

    first commit

---
+++ FontAwesome5.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace B2\\Chars;
+
+class FontAwesome5
+{
+	var $edit_char = '<i class="far fa-edit"></i>';
+	function _edit_str_def() { return '<i class="far fa-edit"></i> '._('Edit'); }
+}
